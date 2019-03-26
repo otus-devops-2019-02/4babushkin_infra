0f4f55e0a3f8073e1ed5ceaa5920cb5549b7db46
Vladimir Babushkin <4babushkin@gmail.com>
2019-03-26 17:03:27 +0700

    add var. add immutable.json

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-gcloud compute instances create reddit-base\
-  --image=reddit-base-1553575349 \
+gcloud compute instances create reddit-full \
+  --image=reddit-full-1553593980 \
   --tags puma-server \
   --restart-on-failure \
  
+#gcloud compute instances create reddit-base\
+#  --image=reddit-base-1553575349 \
+#  --tags puma-server \
+#  --restart-on-failure \
 
---
+++ packer/scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#echo $1
+dir=~/
+echo "Копируем код приложения"
+git clone -b monolith https://github.com/express42/reddit.git
+echo "Устанавливаем зависимости приложения"
+cd reddit && bundle install
+//puma -d
+
+mv /home/$1/puma.service /etc/systemd/system/puma.service
+systemctl start puma
+systemctl enable puma
+echo "...complete."
+
